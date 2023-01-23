cb9d9c6bf3ab7b221b57aa8133ae7142f1578ecf
Azmir <38859114+fahim32446@users.noreply.github.com>
2023-01-23 14:26:15 +0600

    Update url.js

--- new_cl/src/const/url.js
+++ new_cl/src/const/url.js
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:5000/api/v1"
+// export const URL = "http://localhost:5000/api/v1"
 
-// export const URL = "https://hotel-booking-api.onrender.com/api"
+export const URL = "https://hotel-booking-api.onrender.com/api/v1"
